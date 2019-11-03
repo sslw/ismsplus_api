f06f7ba822b16e0a6254cfa5ff99585c93497855
Latif <abdul.latif@sslwireless.com>
2019-11-03 17:22:53 +0600

    add function doc

--- src/lib/SmsInterface.php
+++ src/lib/SmsInterface.php
@@ -6,13 +6,50 @@ namespace Ssl\Isms;
 
 interface SmsInterface
 {
+    /**
+     * @param $msisdn
+     * @param $messageBody
+     * @param $csmsId
+     * @return mixed
+     */
     public function makeParams($msisdn, $messageBody, $csmsId);
+
+    /**
+     * @return mixed
+     */
     public function callApi();
+
+    /**
+     * @param $apiKey
+     * @return void
+     */
     public function setApiToken($apiKey);
+
+    /**
+     * @return string
+     */
     public function getApiToken();
+
+    /**
+     * @param $sid
+     * @return void
+     */
     public function setSid($sid);
+
+    /**
+     * @return mixed
+     */
     public function getSid();
+
+    /**
+     * @param $url
+     * @return void
+     */
     public function setUrl($url);
+
+    /**
+     * @return mixed
+     */
     public function getUrl();
 
 }
